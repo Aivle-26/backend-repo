5d7d5a7cc9e3748e50d69521db9c0c18192e07c4
kim <minsic2000@gmail.com>
2026-07-24 13:08:28 +0900

    feat: configure S3 client with EC2 IAM role

---
+++ src/main/java/com/aivle26/aipm/Config/S3Config.java
@@ -0,0 +1,21 @@
+package com.aivle26.aipm.Config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@EnableConfigurationProperties(S3Properties.class)
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client(S3Properties properties) {
+        return S3Client.builder()
+                .region(Region.of(properties.getRegion()))
+                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .build();
+    }
+}
